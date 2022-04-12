bbabf1d342c481945feeee77210d952902405681
shaheemMPM <shaheem.mpm@gmail.com>
2022-04-12 12:13:01 +0530

    Setup drag drop context

--- src/index.js
+++ src/index.js
@@ -3,20 +3,25 @@ import ReactDOM from 'react-dom';
 import Column from './Column';
 import initialData from './initial-data';
 import '@atlaskit/css-reset';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 const App = () => {
 	// const [data, setData] = useState(initialData);
 	const data = initialData;
 
+	const dragEndHandler = (result) => {
+		console.log('test: ', result);
+	};
+
 	return (
-		<>
+		<DragDropContext onDragEnd={dragEndHandler}>
 			{data.columnOrder.map((columnId) => {
 				const column = data.columns[columnId];
 				const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
 
 				return <Column key={column.id} column={column} tasks={tasks} />;
 			})}
-		</>
+		</DragDropContext>
 	);
 };
 
