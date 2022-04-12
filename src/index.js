b5559cec9f2bb031023d9453e35e13b45e420ffb
shaheemMPM <shaheem.mpm@gmail.com>
2022-04-12 12:41:28 +0530

    Adds additional styling options with dragStart and dragUpdate handlers

--- src/index.js
+++ src/index.js
@@ -8,7 +8,23 @@ import { DragDropContext } from 'react-beautiful-dnd';
 const App = () => {
 	const [data, setData] = useState(initialData);
 
+	// const dragStartHandler = () => {
+	// 	document.body.style.color = 'orange';
+	// 	document.body.style.transition = 'background-color 0.2s ease';
+	// };
+
+	// const dragUpdateHandler = (update) => {
+	// 	const { destination } = update;
+	// 	const opacity = destination
+	// 		? destination.index / Object.keys(data.tasks).length
+	// 		: 0;
+	// 	document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+	// };
+
 	const dragEndHandler = (result) => {
+		// document.body.style.color = 'inherit';
+		// document.body.style.backgroundColor = 'inherit';
+
 		const { source, destination, draggableId } = result;
 
 		if (!destination) return;
@@ -41,7 +57,11 @@ const App = () => {
 	};
 
 	return (
-		<DragDropContext onDragEnd={dragEndHandler}>
+		<DragDropContext
+			// onDragStart={dragStartHandler}
+			// onDragUpdate={dragUpdateHandler}
+			onDragEnd={dragEndHandler}
+		>
 			{data.columnOrder.map((columnId) => {
 				const column = data.columns[columnId];
 				const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
